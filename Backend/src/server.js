1e2fac802918be66726bb82e69e3a410e414259b
Deepak <deepakkumarsahu242@gmail.com>
2026-03-31 08:23:22 +0530

    update cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -49,4 +49,4 @@ app.use("/api/videos", require("./routes/videoRoutes"));
 
 /* ✅ Start Server */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server is running on ${PORT}`));
